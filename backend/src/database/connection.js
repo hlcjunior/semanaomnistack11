a04ac2292e09d4d25eedfd3a9e9fa5d48c1b69e8
hlcjunior <hudson.exe@gmail.com>
2020-03-29 08:58:47 -0300

    app final

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,9 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+//Pega a variável ambiente para saber se vai trocar o banco para o de teste
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
+
+const connection = knex(config);
 
 module.exports = connection;
